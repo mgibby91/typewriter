e3a2b2b6f35105c91d56d20c301bf25f35d30ec4
Matthew Gibson <mattgibson6@gmail.com>
2020-07-27 19:28:55 -0600

    typewriter intial functionality complete to display animation of sentence

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,11 @@
 const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+  }, 100 * (i + 1));
+}
+
+setTimeout(() => {
+  console.log();
+}, sentence.length * 100 + 100);
